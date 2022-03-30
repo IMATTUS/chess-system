8de159f6a4057812b1afc1fe07f21a69ef98cf8a
Ibrahim Mattus Neto <2496339+IMATTUS@users.noreply.github.com>
2022-03-30 06:25:04 -0300

    Starting creation of Piece and Board class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,13 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Chess System by IMATTUS");
-		Position pos = new Position(3, 5);
-		
-		System.out.println(pos.toString());
+		Board board = new Board(8,8);
 	}
 
 }
