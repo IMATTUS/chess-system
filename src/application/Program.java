ccdc9b4f7b7724182add665b458156b727990c4f
Ibrahim Mattus Neto <2496339+IMATTUS@users.noreply.github.com>
2022-04-06 06:53:29 -0300

    Handling captured pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -14,23 +16,28 @@ public class Program {
 		System.out.println("Chess System by IMATTUS");
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 		while(true) {
 			try {
 				UI.clearScreen();
 				System.out.println("");
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch,captured);
 				System.out.println("");
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 				
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
+				System.out.println("");
 				UI.printBoard(chessMatch.getPieces(),possibleMoves);
 				
 				System.out.println("");
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 				ChessPiece capturedPiece = chessMatch.performChessMove(source,target);
+				if(capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
 			}catch(ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
